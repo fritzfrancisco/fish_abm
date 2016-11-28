2cfdb9f7f762e2ea354c4d08414e30f7c2fa7b9a
PaulNueh <paul.nuehrenberg@uni.kn>
2016-11-28 11:59:51 +0100

    version1.1.1

--- fish_mod.cpp
+++ fish_mod.cpp
@@ -16,11 +16,11 @@ using namespace cv;
 
 class Individuals {
 	public:
-		double coords[20][2]; // height n and width dim
-		double dir[20]; // fish direction between 0 and 360°
-		int lag[20]; // with length n
-		// bool state[20]; // 0 for random, 1 for directed
-		int color[20][3];
+		double coords[30][2]; // height n and width dim
+		double dir[30]; // fish direction between 0 and 360°
+		int lag[30]; // with length n
+		bool state[30]; // 0 for random, 1 for directed
+		int color[30][3];
 		int n;
 		int dim;
 };
@@ -31,7 +31,7 @@ void initialize(Individuals& inds, int n, int dim);
 void drawfish(Individuals inds, Mat image);
 int follow(Individuals inds, int id);
 bool state(Individuals inds, int id);
-double border(Individuals inds, int id, double newdir);
+double collision(Individuals inds, int id, double newdir);
 void correctangle(double& dir);
 
 int main(){
@@ -42,9 +42,9 @@ int main(){
 	Mat fish_image = Mat::zeros(h, w, CV_8UC3);
 		
 	Individuals fish;
-	initialize(fish, 20, 2);
+	initialize(fish, 30, 2);
 	
-	for (int z = 0; z < 900; z++){
+	for (int z = 0; z < 1500; z++){
 		Mat fish_image = Mat::zeros(h, w, CV_8UC3);
 		drawfish(fish, fish_image);
 		move(fish);
@@ -60,7 +60,8 @@ int main(){
 void move(Individuals& inds){
 	for (int i = 0; i < inds.n; i++){
 		double dir = inds.dir[i];
-		if(state(inds, i) == 0){
+		inds.state[i] = state(inds, i); 
+		if(inds.state[i] == 0){
 			dir = inds.dir[i] + rand() % 91 - 45; // feeding, new fish direction
 			if(inds.lag[i] == 0){
 				inds.lag[i] = 10;
@@ -70,7 +71,7 @@ void move(Individuals& inds){
 			}
 		}		
 		correctangle(dir);		
-		double newdir = border(inds, i, dir);		
+		double newdir = collision(inds, i, dir);		
 
 		inds.coords[i][0] = inds.coords[i][0] + (state(inds, i) * 4 + 1) * speed * cos(newdir * PI / 180);
 		inds.coords[i][1] = inds.coords[i][1] + (state(inds, i) * 4 + 1) * speed * sin(newdir * PI / 180);
@@ -94,7 +95,7 @@ void initialize(Individuals& inds, int n, int dim){
 		inds.color[i][0] = 120 + rand() % 120;
 		inds.color[i][1] = 120 + rand() % 120;
 		inds.color[i][2] = 120 + rand() % 120;
-		inds.dir[i] = rand() % 360; // direction
+		inds.dir[i] = 0; // rand() % 360; // direction
 		for (int u = 0; u < inds.dim; u++){
 			if (u == 0) {
 				inds.coords[i][u] = w / 2 + rand() % 201 - 100; // x coord
@@ -121,18 +122,19 @@ int follow(Individuals inds, int id){
 	int c =  0; // count number of inds in front of id
 	double idcoords[2] = {inds.coords[id][0], inds.coords[id][1]};
 	for (int i = 0; i < inds.n; i++){
-		double dist[20]; // absolut distance between fish i and fish id
-		double dirvector[20][2]; // direction vector of lenth 1
-		double angle[20]; // angle between fish id direction vector and fish i pos vector
+		double dist[inds.n]; // absolut distance between fish i and fish id
+		double dirvector[2]; // direction vector (fish id) of lenth 1
+		double angle[inds.n]; // angle between fish id direction vector and fish i pos vector
 
 		inds.coords[i][0] = inds.coords[i][0] - idcoords[0]; // set fish id to x = 0
 		inds.coords[i][1] = inds.coords[i][1] - idcoords[1]; // set fish id to y = 0
-		dist[i] = sqrt(pow(inds.coords[i][0], 2) + pow(inds.coords[i][1], 2));
-		dirvector[i][0] = cos(inds.dir[i] * PI / 180);
-		dirvector[i][1] = sin(inds.dir[i] * PI / 180);
+		dist[i] = sqrt(pow(inds.coords[i][0], 2) + pow(inds.coords[i][1], 2)); // calculate absolute distances of respective i to id
+		dirvector[0] = cos(inds.dir[i] * PI / 180);
+		dirvector[1] = sin(inds.dir[i] * PI / 180);
 		
 		if (i != id){
-			angle[i] =  acos((dirvector[i][0] * inds.coords[i][0] + dirvector[i][1] * inds.coords[i][1]) / dist[i]) * 180 / PI;
+			// dot product(direction vector of id, position vector of i)
+			angle[i] =  acos((dirvector[0] * inds.coords[i][0] + dirvector[1] * inds.coords[i][1]) / dist[i]) * 180 / PI; 
 		}
 		else{
 			angle[i] = 180; // never see yourself!	
@@ -154,34 +156,41 @@ bool state(Individuals inds, int id){
 	return state;
 }
 
-double border(Individuals inds, int id, double newdir){
-	double bound = 20;
-	double leftcoords[2];	
-	double rightcoords[2];	
-	leftcoords[0] = inds.coords[id][0] + (state(inds, id) * 4 + 1) * speed * cos(newdir * PI / 180); // "new" x coord after left turn
-	leftcoords[1] = inds.coords[id][1] + (state(inds, id) * 4 + 1) * speed * sin(newdir * PI / 180); // "new" y coord
-	rightcoords[0] = inds.coords[id][0] + (state(inds, id) * 4 + 1) * speed * cos(newdir * PI / 180); // "new" x coord after right turn
-	rightcoords[1] = inds.coords[id][1] + (state(inds, id) * 4 + 1) * speed * sin(newdir * PI / 180); // "new" y coord
+double collision(Individuals inds, int id, double newdir){
+	double bound = 20; // boundary in which to detect the border
 
-	double newdirleft = newdir;
+	double leftcoords[2]; // new possible coords after left turn
+	double rightcoords[2]; // new possible coords after right turn
+
+	leftcoords[0] = inds.coords[id][0] + (state(inds, id) * 4 + 1) * speed * cos(newdir * PI / 180); // initialize new coords after turn with original values
+	leftcoords[1] = inds.coords[id][1] + (state(inds, id) * 4 + 1) * speed * sin(newdir * PI / 180);
+
+	rightcoords[0] = inds.coords[id][0] + (state(inds, id) * 4 + 1) * speed * cos(newdir * PI / 180);
+	rightcoords[1] = inds.coords[id][1] + (state(inds, id) * 4 + 1) * speed * sin(newdir * PI / 180);
+
+	double newdirleft = newdir; // new direction after turning (left/right), initialized with original direction
 	double newdirright = newdir;
 
+	// check collision after turn (left or right, 0 for no collision, 1 for collision); starting with original direction..
 	bool turnleft = (leftcoords[0] < bound || leftcoords[0] > (w - bound) || leftcoords[1] < bound || leftcoords[1] > (h - bound));
 	bool turnright = (rightcoords[0] < bound || rightcoords[0] > (w - bound) || rightcoords[1] < bound || rightcoords[1] > (h - bound));
 
+	// .. then adjust turn direction and check collision again; repeat until one collision statement (left/right) is 0 (no collision):
 	while(turnleft && turnright){
 		newdirleft = newdirleft - 1;	
-		leftcoords[0] = inds.coords[id][0] + (state(inds, id) * 4 + 1) * speed * cos(newdirleft * PI / 180); // "new" x coord after left turn
-		leftcoords[1] = inds.coords[id][1] + (state(inds, id) * 4 + 1) * speed * sin(newdirleft * PI / 180); // "new" y coord
+		leftcoords[0] = inds.coords[id][0] + (state(inds, id) * 4 + 1) * speed * cos(newdirleft * PI / 180); // new x coord after left turn
+		leftcoords[1] = inds.coords[id][1] + (state(inds, id) * 4 + 1) * speed * sin(newdirleft * PI / 180); // new y coord
 
 		newdirright = newdirright + 1;	
-		rightcoords[0] = inds.coords[id][0] + (state(inds, id) * 4 + 1) * speed * cos(newdirright * PI / 180); // "new" x coord after right turn
-		rightcoords[1] = inds.coords[id][1] + (state(inds, id) * 4 + 1) * speed * sin(newdirright * PI / 180); // "new" y coord
+		rightcoords[0] = inds.coords[id][0] + (state(inds, id) * 4 + 1) * speed * cos(newdirright * PI / 180); // new x coord after right turn
+		rightcoords[1] = inds.coords[id][1] + (state(inds, id) * 4 + 1) * speed * sin(newdirright * PI / 180); // new y coord
 
 		turnleft = (leftcoords[0] < bound || leftcoords[0] > (w - bound) || leftcoords[1] < bound || leftcoords[1] > (h - bound));
 		turnright = (rightcoords[0] < bound || rightcoords[0] > (w - bound) || rightcoords[1] < bound || rightcoords[1] > (h - bound));
 	}
-	if (turnright){
+	
+	// choose new direction based on false collision statement (no collision in this direction):
+	if (turnleft == 0){
 		newdir = newdirleft;
 	}
 	else{
